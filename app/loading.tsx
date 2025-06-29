c676246d72717a3dc7bc948b63b430c0ec6c4926
hemanth5055 <kanalahemanthrdy@gmail.com>
2025-06-29 11:32:13 +0530

    Fixed flickering

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center ">
       <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
     </div>
   );
